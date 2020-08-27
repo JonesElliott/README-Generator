7ee0fbbd7a4728184a2fc829fcb6b02bbf6ed7c7
Elliott Jones <jones.elliott@outlook.com>
2020-08-26 19:22:39 -0700

    Created function to request deployed url & description with multi paragraph option

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
+// Variable Declarations
 const fs = require("fs");
-// const axios = require("axios");
 const inquirer = require("inquirer");
+var pNum = 1; // to display paragraph numbers
 
 // Kick off the program
 getTitle();
 
+// Object to store all user entered data for easy access
 var userParams = {
     title: "",
+    deployedURL: "",
     badge: [],
-    description: "",
-    installation: "",
-    usage: "",
-    license: "",
-    contributions: "",
-    tests: "",
-    support: ""
+    description: [],
+    installation: [],
+    usage: [],
+    license: [],
+    contributions: [],
+    tests: [],
+    support: []
 }
 
-// Request title
+// Request project's title
 function getTitle() {
     inquirer
       .prompt([
         {
-            message: "Enter project Title: ",
+            message: "Enter the project's Title: ",
             name: "title"
         }]).then(function({ title }) {
             userParams.title = title;
-            console.log(title);
-            console.log(userParams.title);
-            getBadge();
+            getDeployedURL();
         });
 }
 
+// Request deployed URL for project
+function getDeployedURL() {
+    inquirer
+      .prompt([
+        {
+            message: "Enter the Deployed URL: ",
+            name: "deployedURL"
+        }]).then(function({ deployedURL }){
+            userParams.deployedURL = deployedURL;
+            getBadge();
+        })
+}
+
+// Request any badge(s)
 function getBadge() {
     inquirer
       .prompt([
@@ -43,22 +58,39 @@ function getBadge() {
             type: 'list',
             message: "Add another badge?",
             choices: ["Yes", "No"],
-            name: "choiceBadge"
-        }]).then(function({ badge, choiceBadge }){
+            name: "choice"
+        }]).then(function({ badge, choice }){
             userParams.badge.push(badge);
-            if (choiceBadge === "Yes") {
-                console.log(badge);
-                console.log("Do it again...");
+            if (choice === "Yes") {
                 getBadge();
             } else {
-                console.log(badge);
-                console.log(userParams.badge);
-                console.log("Done... Yo It worked!");
+                getDescription();
             }
         })
 }
 
-
-
-
+// Request description paragraph(s)
+function getDescription() {
+    inquirer
+      .prompt([
+        {
+            message: "Description | Paragraph #" + pNum + ": ",
+            name: "description"
+        },
+        {   // This gives the user an option to add another badge or continue
+            type: 'list',
+            message: "Add another paragraph?",
+            choices: ["Yes", "No"],
+            name: "choice"
+        }]).then(function({ description, choice }){
+            userParams.description.push(description);
+            pNum++;
+            // console.log(pNum);
+            if (choice === "Yes") {
+                getDescription();
+            } else {
+                pNum = 1;
+            }
+        })
+}
 
