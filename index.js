d988aa7666f7083a740ff590a2e84624f7e4fd59
Elliott Jones <jones.elliott@outlook.com>
2020-08-26 22:44:19 -0700

    Added function to select predefined license or add user defined license

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Variable Declarations
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
+const Choices = require("inquirer/lib/objects/choices");
+const Choice = require("inquirer/lib/objects/choice");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 var pNum = 1; // to display paragraph numbers
 
 // Kick off the program
@@ -14,10 +19,10 @@ var userParams = {
     description: [],
     installation: [],
     usage: [],
-    license: [],
     contributions: [],
     tests: [],
-    support: []
+    support: [],
+    license: []
 }
 
 // Request project's title
@@ -77,7 +82,7 @@ function getDescription() {
             message: "Description | Paragraph #" + pNum + ": ",
             name: "description"
         },
-        {   // This gives the user an option to add another badge or continue
+        {   // This gives the user an option to add another paragraph or continue
             type: 'list',
             message: "Add another paragraph?",
             choices: ["Yes", "No"],
@@ -85,12 +90,152 @@ function getDescription() {
         }]).then(function({ description, choice }){
             userParams.description.push(description);
             pNum++;
-            // console.log(pNum);
             if (choice === "Yes") {
                 getDescription();
             } else {
                 pNum = 1;
+                getInstallation();
             }
         })
 }
 
+// Request installation paragraph(s)
+function getInstallation() {
+    inquirer
+      .prompt([
+        {
+            message: "Installation | Paragraph #" + pNum + ": ",
+            name: "installation"
+        },
+        {   // This gives the user an option to add another paragraph or continue
+            type: 'list',
+            message: "Add another paragraph?",
+            choices: ["Yes", "No"],
+            name: "choice"
+        }]).then(function({ installation, choice }){
+            userParams.installation.push(installation);
+            pNum++;
+            if (choice === "Yes") {
+                getInstallation();
+            } else {
+                pNum = 1;
+                getUsage();
+            }
+        })
+}
+
+// Request usage paragraph(s)
+function getUsage() {
+    inquirer
+      .prompt([
+        {
+            message: "Usage | Paragraph #" + pNum + ": ",
+            name: "usage"
+        },
+        {   // This gives the user an option to add another paragraph or continue
+            type: 'list',
+            message: "Add another paragraph?",
+            choices: ["Yes", "No"],
+            name: "choice"
+        }]).then(function({ usage, choice }){
+            userParams.usage.push(usage);
+            pNum++;
+            if (choice === "Yes") {
+                getUsage();
+            } else {
+                pNum = 1;
+                getContributions();
+            }
+        })
+}
+
+// Request contribution paragraph(s)
+function getContributions() {
+    inquirer
+      .prompt([
+        {
+            message: "Contributions | Paragraph #" + pNum + ": ",
+            name: "contributions"
+        },
+        {   // This gives the user an option to add another paragraph or continue
+            type: 'list',
+            message: "Add another paragraph?",
+            choices: ["Yes", "No"],
+            name: "choice"
+        }]).then(function({ contributions, choice }){
+            userParams.contributions.push(contributions);
+            pNum++;
+            if (choice === "Yes") {
+                getContributions();
+            } else {
+                pNum = 1;
+                getLicense();
+            }
+        })
+}
+
+// Request license information
+function getLicense() {
+    inquirer
+      .prompt([
+        {   // This gives the user preset license options 
+            type: 'list',
+            message: "Select a license:",
+            choices: [
+            "MIT", 
+            "GNU-GPLv3", 
+            "Mozilla", 
+            "Apache", 
+            "Boost", 
+            "Unlicense", 
+            "I'll input my own",
+            ],
+            name: "licenseChoice"
+        }]).then(function({ licenseChoice }){
+            if (licenseChoice === "I'll input my own") {
+                // Get user defined license
+                getLicenseInput(); 
+            } else {
+                // Set predefined license
+                readLicense(licenseChoice);
+            }
+        })
+}
+
+// retrieve the selected license
+async function readLicense(licenseChoice) {
+    try {
+      // Read from each file
+      const licenseData = await readFileAsync('./license-options/' + licenseChoice + '.txt', 'utf8');
+      userParams.license = licenseData;
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+// Request user defined license
+function getLicenseInput() {
+    inquirer
+      .prompt([
+        {
+            message: "License | Paragraph #" + pNum + ": ",
+            name: "license"
+        },
+        {   // This gives the user an option to add another paragraph or continue
+            type: 'list',
+            message: "Add another paragraph?",
+            choices: ["Yes", "No"],
+            name: "choice"
+        }]).then(function({ license, choice }){
+            userParams.usage.push(license);
+            pNum++;
+            if (choice === "Yes") {
+                getLicenseInput();
+            } else {
+                pNum = 1;
+                // Move on
+            }
+        })
+}
+
+  
